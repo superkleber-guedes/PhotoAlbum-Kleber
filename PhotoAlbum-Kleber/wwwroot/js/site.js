fd1d4912df7b2d5f580a96cb301ccbe0fc1e4775
Kleber Guedes <kleber.guedes@gmail.com>
2018-08-15 18:38:21 +0100

    Adding paging - Finished

--- PhotoAlbum-Kleber/wwwroot/js/site.js
+++ PhotoAlbum-Kleber/wwwroot/js/site.js
@@ -1,14 +1,4 @@
 ﻿// Write your JavaScript code.
 $(document).ready(function () {
-    $("#txtSearch").keyup(function () {
-        var searchText = $("#txtSearch").val();
-        $("#photoTable tbody tr").each(function () {
-            var tr = $(this);
-            if (!tr.text().toLowerCase().includes(searchText.toLowerCase())) {
-                tr.addClass("hide");
-            } else {
-                tr.removeClass("hide");
-            };
-        });
-    });
+    $('#photoTable').DataTable();
 });
